daae2ec083d56970f331ba9de893185cac3dae3a
Elieren <kir102906@gmail.com>
2023-01-13 15:49:06 +0700

    Version 0.8

--- RFID.py
+++ RFID.py
@@ -40,7 +40,9 @@ if level == 1:
             print(f"Card read UID: {uid[0]}.{uid[1]}.{uid[2]}.{uid[3]}")
             key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
             MIFAREReader.MFRC522_SelectTag(uid)
-            MIFAREReader.MFRC522_DumpClassic1K(key, uid)
+            text = MIFAREReader.MFRC522_DumpClassic1K(key, uid)
+            for x in text:
+                print(x[0])
             MIFAREReader.MFRC522_StopCrypto1()
 
 elif level == 2:
@@ -79,11 +81,47 @@ elif level == 2:
 
                 MIFAREReader.MFRC522_Write(0, data)
                 print('Now it looks like this:')
-                MIFAREReader.MFRC522_Read(0)
+                text = MIFAREReader.MFRC522_Read(0)
+                print(text[0])
                 MIFAREReader.MFRC522_StopCrypto1()
 
 elif level == 3:
-    pass
+    signal.signal(signal.SIGINT, end_read)
+    MIFAREReader = MFRC522.MFRC522()
+    (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    if status == MIFAREReader.MI_OK:
+        print("Card detected")
+        (status, uid) = MIFAREReader.MFRC522_Anticoll()
+
+        if status == MIFAREReader.MI_OK:
+            data = []
+
+            print(f"Card read UID: {uid[0]}.{uid[1]}.{uid[2]}.{uid[3]}")
+            key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+            MIFAREReader.MFRC522_SelectTag(uid)
+
+            #All read tag
+            print('Attach the tag you want to copy.')
+            text = MIFAREReader.MFRC522_DumpClassic1K(key, uid)
+            for x in text:
+                print(x[0])
+                data.append(x[0])
+            
+            print('Label read remove it, press enter and attach the label to which you want to copy.')
+            input('Enter ->')
+
+            #All write tag
+            print('Attach a label.')
+            (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+            if status == MIFAREReader.MI_OK:
+                print("Card detected")
+                try:
+                    MIFAREReader.MFRC522_WriteClassic1K(key, uid, data)
+                    print('Written')
+                except:
+                    print('Error')
 
 elif level == 4:
     reader = MFRC522.SimpleMFRC522()
