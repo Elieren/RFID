95fb7ebf3ccc59796ad979c1d03cdde73da06e75
Elieren <kir102906@gmail.com>
2023-01-19 11:25:38 +0700

    Version 1.1

--- RFID.py
+++ RFID.py
@@ -107,13 +107,14 @@ elif level == 2:
             time.sleep(1)
 
 elif level == 3:
-    numder = int(input('Change sector values: '))
-    data = []
-    print(f'Enter new values for sector {numder}')
     print('Example:12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0')
     print('----------------------Disclaimer-----------------------')
     print('It is not recommended to change sectors 3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63, as this may lead to card failure.')
     print('-------------------------------------------------------')
+    print()
+    numder = int(input('Change sector values: '))
+    data = []
+    print(f'Enter new values for sector {numder}')
     new_uid = str(input(': '))
     new_uid_t = new_uid.split(', ')
     for x in new_uid_t:
